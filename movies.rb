6448146411418b65c898897bb75badb2e4ac3b72
Patrick Hanly <phanly@formationit.com>
2014-08-21 15:57:16 -0400

    Basics Ex 3

---
+++ movies.rb
@@ -0,0 +1,7 @@
+movies = {"Jaws" => 1975, "Troy" => 2004, "ManofSteel" => 2013, "LOTR" => 2001, "Excalibur" => 1981}
+
+puts movies["Jaws"]
+puts movies["Troy"]
+puts movies["ManofSteel"]
+puts movies["LOTR"]
+puts movies["Excalibur"]
