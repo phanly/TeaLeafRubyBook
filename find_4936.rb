ebd2a06cc17306c8498b5bde348d371c97e453e4
Patrick Hanly <phanly@formationit.com>
2014-08-21 15:23:31 -0400

    Basics Ex 2

---
+++ find_4936.rb
@@ -0,0 +1,9 @@
+thousands = 4936 / 1000
+hundreds = 4936 / 100
+tens = 4936 / 10
+ones = 4936 / 1
+
+puts "Thousands is ", thousands
+puts "Hundreds is ", hundreds
+puts "Tens is ", tens
+puts "Ones is ", ones
