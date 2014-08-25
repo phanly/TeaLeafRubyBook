ceacafcfb2bc06eced918e248133057b46461000
Patrick Hanly <phanly@formationit.com>
2014-08-25 13:21:57 -0400

    Flow Control Ex 5 #2

--- eval_number5.rb
+++ eval_number5.rb
@@ -3,18 +3,26 @@
 puts "Enter a number between 0 and 100 : "
 i = gets.to_i
 
-case
-when i <= 49
-  puts "Your number is between 0 and 50"
-  exit
+def eval49(i)
+  case
+  when i <= 49
+    puts "Your number is between 0 and 50"
+    exit
+  end
 end
 
-case
-when i <= 100
-  puts "Your number is between 50 and 100"
-when i > 100
-  puts "Your number is greater than 100"
+def eval100(i)
+  case
+  when i <= 100
+    puts "Your number is between 50 and 100"
+  when i > 100
+    puts "Your number is greater than 100"
+  end
 end
+
+eval49(i)
+eval100(i)
+  
 
 
   
