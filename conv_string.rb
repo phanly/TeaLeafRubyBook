04c4c50fc25e054979f9ed7ed025508f0c894f41
Patrick Hanly <phanly@formationit.com>
2014-08-25 11:53:06 -0400

    Flow Contol Ex 2

---
+++ conv_string.rb
@@ -0,0 +1,15 @@
+# conv_string.rb
+
+def s_upper(s)
+  if s.length > 10
+    puts s.capitalize
+  elsif
+    puts "String less than 10 characters"
+  end
+end
+
+s_upper("This is my string")
+  
+
+  
+  
