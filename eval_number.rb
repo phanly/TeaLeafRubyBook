a4fb74def018f037f508b1c7fa56f33b7fe2545b
Patrick Hanly <phanly@formationit.com>
2014-08-25 11:27:33 -0400

    Flow Control Ex 3

---
+++ eval_number.rb
@@ -0,0 +1,19 @@
+# eval_number.rb
+
+puts "Enter a number between 0 and 100 : "
+i = gets.to_i
+  
+if i <= 49
+  puts "Your number is between 0 and 50"
+  exit
+end
+
+if i <= 100
+  puts "Your number is between 50 and 100"
+elsif 
+  puts "Your number is greater than 100"
+end
+
+
+  
+  
