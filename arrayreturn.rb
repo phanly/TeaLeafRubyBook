1a8a86b0e8e7e7a3fe3cae412f6587891a5d3e70
Patrick Hanly <phanly@formationit.com>
2014-08-26 11:27:28 -0400

    Arrays Ex 4

---
+++ arrayreturn.rb
@@ -0,0 +1,16 @@
+# arrayretun.rb v1.0 - Arrays Ex 4
+
+arr = [15, 7, 18, 5, 12, 8, 5, 1]
+
+a = arr.index(5)
+
+b = arr.index[5]
+
+c = arr[5]
+
+puts a
+# 3
+puts b
+#NoMethodError: undefined method `[]' for #<Enumerator: [15, 7, 18, 5, 12, 8, 5, 1]:index>
+puts c
+# 8
