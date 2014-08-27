57548b42b8b69d17aa42bf0c62ed93b1d749deba
Patrick Hanly <phanly@formationit.com>
2014-08-27 17:26:59 -0400

    Hashes Ex 7

---
+++ hashdiff.rb
@@ -0,0 +1,9 @@
+# hashdiff.rb v1.0 - Hashes Ex 7
+# find difference between two hashes
+
+x = "hi there"
+my_hash = {x: "some value"}
+my_hash2 = {x => "some value"}
+
+# x in the first hash is a symbol
+# x in the second hash is a string value. x is mostly likely a variable
