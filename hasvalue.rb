64e4eb541c93d5f6f2ec9caf64f3826f844cfcc9
Patrick Hanly <phanly@formationit.com>
2014-08-27 17:07:03 -0400

    Hashes Ex 5

---
+++ hasvalue.rb
@@ -0,0 +1,10 @@
+# hasvalue.rb v1.0 - Hashes Ex 4
+# determine if hash has a particular value
+
+# method to use is has_value?
+
+family = { Dad: 'Pat', Mom: 'Liz', Daughter1: 'Caitlyn', Son: 'Patrick', Daughter2: 'Jaimee'}
+
+value = family.has_value?('Caitlyn')
+
+puts value # will return true
