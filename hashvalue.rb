15798240298a7fe0909efa67dd7b4bfd1e53c5ff
Patrick Hanly <phanly@formationit.com>
2014-08-27 17:02:13 -0400

    Hashes Ex 4

---
+++ hashvalue.rb
@@ -0,0 +1,8 @@
+# hashvalue.rb v1.0 - Hashes Ex 4
+# print the name of the person from the hash
+
+person = {name: 'Bob', occupation: 'web developer', hobbies: 'painting'}
+
+name = person[:name]
+
+puts name
